c9489b12167bb4e5e5d9349967d3abd3e5dcc493
cjwcommuny <cjwcommuny@outlook.com>
2022-04-06 19:17:29 +0800

    refactor

--- clock-in.py
+++ clock-in.py
@@ -1,188 +1,107 @@
-# -*- coding: utf-8 -*-
-
-# 打卡脚修改自ZJU-nCov-Hitcarder的开源代码，感谢这位同学开源的代码
-import ast
-
-import requests
+import datetime
 import json
 import re
-import datetime
-import time
 import sys
+import time
+from copy import copy
+from dataclasses import dataclass
+from typing import Callable
 
-from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from requests import Response, Session
 
-def is_dict(obj: str) -> bool:
-    obj = obj.replace(' ', '')
-    return obj[1] == '"' or obj[1] == '\''
+LOGIN_URL = 'https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex'
+PUBLIC_KEY_URL = 'https://zjuam.zju.edu.cn/cas/v2/getPubKey'
+BASE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
+SAVE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
 
-def parse_dict(obj: str) -> dict:
-    return json.loads(obj)
+@dataclass
+class Rsa:
+    modulus: str
+    exponent: str
 
-def parse_json_obj(obj: str) -> dict:
-    from argparse import Namespace
-    obj = obj.strip('{} ')
-    obj = obj.replace(':', '=')
-    to_evaluated = f'Namespace({obj})'
-    return vars(eval(to_evaluated))
+def generate_headers() -> dict:
+    return {'user-agent': UserAgent().chrome}
 
-class DaKa(object):
-    """Hit card class
+def get_execution(session: Session, login_url: str) -> str:
+    response: Response = session.get(login_url)
+    execution: str = re.search('name="execution" value="(.*?)"', response.text).group(1)
+    return execution
 
-    Attributes:
-        username: (str) 浙大统一认证平台用户名（一般为学号）
-        password: (str) 浙大统一认证平台密码
-        login_url: (str) 登录url
-        base_url: (str) 打卡首页url
-        save_url: (str) 提交打卡url
-        sess: (requests.Session) 统一的session
-    """
+def get_rsa_info(session: Session, public_key_url: str) -> Rsa:
+    return Rsa(**session.get(public_key_url).json())
 
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
-    }
+def rsa_encrypt(message: str, rsa_info: Rsa) -> str:
+    password_bytes = bytes(message, 'ascii')
+    password_int = int.from_bytes(password_bytes, 'big')
+    e_int = int(rsa_info.exponent, 16)
+    M_int = int(rsa_info.modulus, 16)
+    result_int = pow(password_int, e_int, M_int)
+    return hex(result_int)[2:].rjust(128, '0')
 
-    LOGIN_URL = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
-    BASE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
-    SAVE_URL = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.sess = requests.Session()
-        self.sess.keep_alive = False
-
-    def login(self):
-        """Login to ZJU platform"""
-        res = self.sess.get(self.LOGIN_URL)
-        execution = re.search(
-            'name="execution" value="(.*?)"', res.text).group(1)
-        res = self.sess.get(
-            url='https://zjuam.zju.edu.cn/cas/v2/getPubKey').json()
-        n, e = res['modulus'], res['exponent']
-        encrypt_password = self._rsa_encrypt(self.password, e, n)
-
-        data = {
-            'username': self.username,
-            'password': encrypt_password,
+def login(username: str, password: str) -> Callable[[str, str], Session]:
+    def call(login_url: str, public_key_url: str) -> Session:
+        session = Session()
+        execution = get_execution(session, login_url)
+        rsa_info = get_rsa_info(session, public_key_url)
+        password_encrypted = rsa_encrypt(password, rsa_info)
+        data_to_post = {
+            'username': username,
+            'password': password_encrypted,
             'execution': execution,
             '_eventId': 'submit'
         }
-        res = self.sess.post(url=self.LOGIN_URL, data=data)
+        response = session.post(login_url, data_to_post)
+        if '统一身份认证' in response.content.decode():
+            raise Exception('登录失败')
+        return session
+    return call
 
-        # check if login successfully
-        if '统一身份认证' in res.content.decode():
-            raise LoginError('登录失败，请核实账号密码重新登录')
-        return self.sess
+def get_date() -> str:
+    today = datetime.date.today()
+    return "%4d%02d%02d" % (today.year, today.month, today.day)
 
-    def post(self):
-        """Post the hitcard info"""
-        res = self.sess.post(self.SAVE_URL, data=self.info, headers=self.headers)
-        return json.loads(res.text)
+def generate_info(session: Session, base_url: str) -> dict:
+    response = session.get(base_url, headers=generate_headers())
+    html: str = response.content.decode()
+    old_info_str = re.findall(r'oldInfo: ({.*}),', html)[0]
+    old_info: dict = json.loads(old_info_str)
+    new_info = generate_new_info_from(old_info)
+    return new_info
 
-    def get_info(self, html=None):
-        """Get hitcard info, which is the old info with updated new time."""
-        if not html:
-            res = self.sess.get(self.BASE_URL, headers=self.headers)
-            html = res.content.decode()
-        soup = BeautifulSoup(BeautifulSoup(html, 'html.parser').prettify(), 'html.parser')
-        script_string = soup.find_all('script', type="text/javascript")[-1].string.replace('\n', ' ')
-        obj = re.findall(r'var def = (.*?})(?=;)', script_string)[0]
-        old_info = parse_dict(obj) if is_dict(obj) else parse_json_obj(obj)
-        name = re.findall(r'realname: "([^\"]+)",', html)[0]
-        number = re.findall(r"number: '([^\']+)',", html)[0]
-        #
-        new_info = old_info.copy()
-        # new_info['id'] = new_id
-        new_info['name'] = name
-        new_info['number'] = number
-        new_info["date"] = self.get_date()
-        new_info["created"] = round(time.time())
-        new_info["address"] = "浙江省杭州市西湖区"
-        new_info["area"] = "浙江省 杭州市 西湖区"
-        new_info["province"] = new_info["area"].split(' ')[0]
-        new_info["city"] = new_info["area"].split(' ')[1]
-        # form change
-        new_info['jrdqtlqk[]'] = 0
-        new_info['jrdqjcqk[]'] = 0
-        new_info['sfsqhzjkk'] = 1   # 是否申领杭州健康码
-        new_info['sqhzjkkys'] = 1   # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
-        new_info['sfqrxxss'] = 1    # 是否确认信息属实
-        new_info['jcqzrq'] = ""
-        new_info['gwszdd'] = ""
-        new_info['szgjcs'] = ""
-        self.info = new_info
-        return new_info
+def generate_new_info_from(old_info: dict) -> dict:
+    province, city, region = '浙江省', '杭州市', '西湖区'
+    new_info = copy(old_info)
+    new_info['date'] = get_date()
+    new_info['created'] = round(time.time())
+    new_info["address"] = f'{province}{city}{region}'
+    new_info["area"] = f'{province} {city} {region}'
+    new_info['provinece'] = province
+    new_info['city'] = city
+    new_info['address'] = '浙江省杭州市西湖区灵隐街道浙江大学玉泉校区'
+    new_info['jrdqjcqk'] = 0
+    new_info['sfsqhzjkk'] = 1   # 是否申领杭州健康码
+    new_info['sqhzjkkys'] = 1   # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
+    new_info['sfqrxxss'] = 1    # 是否确认信息属实
+    new_info['jcqzrq'] = ""
+    new_info['gwszdd'] = ""
+    new_info['szgjcs'] = ""
+    # --- new
+    new_info['campus'] = '玉泉校区'
+    return new_info
 
-    def _rsa_encrypt(self, password_str, e_str, M_str):
-        password_bytes = bytes(password_str, 'ascii')
-        password_int = int.from_bytes(password_bytes, 'big')
-        e_int = int(e_str, 16)
-        M_int = int(M_str, 16)
-        result_int = pow(password_int, e_int, M_int)
-        return hex(result_int)[2:].rjust(128, '0')
-
-    @staticmethod
-    def get_date():
-        """Get current date"""
-        today = datetime.date.today()
-        return "%4d%02d%02d" % (today.year, today.month, today.day)
-
-# Exceptions
-class LoginError(Exception):
-    """Login Exception"""
-    pass
+def post_data(session: Session, save_url: str, info: dict):
+    response_text = session.post(save_url, info, headers=generate_headers()).text
+    response = json.loads(response_text)
+    if response['e'] != 0:
+        raise Exception(f'打卡失败: {response["m"]}')
 
 
-class RegexMatchError(Exception):
-    """Regex Matching Exception"""
-    pass
-
-
-class DecodeError(Exception):
-    """JSON Decode Exception"""
-    pass
-
-
-def main(username, password):
-    """Hit card process
-
-    Arguments:
-        username: (str) 浙大统一认证平台用户名（一般为学号）
-        password: (str) 浙大统一认证平台密码
-    """
-    print("\n[Time] %s" %
-          datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    print("🚌 打卡任务启动")
-
-    dk = DaKa(username, password)
-
-    print("登录到浙大统一身份认证平台...")
-    try:
-        dk.login()
-        print("已登录到浙大统一身份认证平台")
-    except Exception as err:
-        print(str(err))
-        raise Exception
-
-    print('正在获取个人信息...')
-    dk.get_info()
-    print('%s %s同学, 你好~' % (dk.info['number'], dk.info['name']))
-
-    print('正在为您打卡')
-    try:
-        res = dk.post()
-        if str(res['e']) == '0':
-            print('已为您打卡成功！')
-        else:
-            print(res['m'])
-    except Exception:
-        print('数据提交失败')
-        raise Exception
-
-
-if __name__ == "__main__":
-    print(sys.argv)
+if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
-    main(username, password)
+    print('开始打卡')
+    session = login(username, password)(LOGIN_URL, PUBLIC_KEY_URL)
+    print('登录成功')
+    new_info = generate_info(session, BASE_URL)
+    print('打卡成功')
